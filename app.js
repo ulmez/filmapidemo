728379596d6f22a42a324dcb2f2be284029dd058
Ulf Zackrisson <ulmezz@hotmail.com>
2020-05-11 16:47:28 +0200

    Installed cors and body-parser

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-/* dfgsdfgsdfgdfsg */
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cors());
+app.options('*', cors());
+
+module.exports = app;
